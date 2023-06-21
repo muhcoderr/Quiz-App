5a7a746ab46b62ea257f61d066e8d22d7ba9c47d
Muhammad Tariq <muhammad4948@gmail.com>
2023-06-21 23:29:41 +0500

    first commit

---
+++ questions.js
@@ -0,0 +1,48 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following attributes is used to add link to any element?",
+    answer: "href",
+    options: [
+      "link",
+      "href",
+      "ref",
+      "newref"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  }
+    
+];
